d03215b62291b2b0de3e333cb5ccfc2488ed5f3c
Alexandre Zampronne Zaccaron Rocha <106710530+AlexandreComn@users.noreply.github.com>
2023-12-11 11:08:44 -0300

    ......

--- modalpasteis.js
+++ modalpasteis.js
@@ -1,41 +1,96 @@
 function abrirModalPasteis(nome, preco, id) {
-    document.getElementById("modal-nome-pasteis").textContent = "Item: " + nome;
-    document.getElementById("modal-preco-pasteis").textContent = "Preço: R$" + preco.toFixed(2);
-    document.getElementById("modal-id-pasteis").value = id;
+    var modal = document.getElementById("modalPasteis");
+    var modalTitle = document.getElementById("modalTitle");
+    var modalPrice = document.getElementById("modalPrice");
+    var modalId = document.getElementById("modal-id");
 
-    // Configurar as opções adicionais com base na categoria
-    document.getElementById("quantidade-adicional-pasteis").value = 0; // Reiniciar quantidade de adicionais
-    document.getElementById("modalPasteis").style.display = "block";
+    modalTitle.textContent = nome;
+    modalPrice.textContent = "Preço: R$" + preco;
+    modalId.value = id;
+
+    var camposAdicionais = document.getElementById("camposAdicionais");
+    camposAdicionais.style.display = "none";
+
+    var quantidadeAdicionais = document.getElementById("quantidadeAdicionais");
+    var descricaoAdicionais = document.getElementById("descricaoAdicionais");
+
+    quantidadeAdicionais.value = 0;
+    descricaoAdicionais.value = "";
+
+    modal.style.display = "block";
 }
 
+function fecharModalPasteis() {
+    var modal = document.getElementById("modalPasteis");
+    modal.style.display = "none";
+}
 
+function mostrarCamposAdicionais() {
+    var tipoAdicional = document.getElementById("additional").value;
+    var camposAdicionais = document.getElementById("camposAdicionais");
+
+    if (tipoAdicional === "3") {
+        camposAdicionais.style.display = "block";
+    } else {
+        camposAdicionais.style.display = "none";
+    }
+}
 
 function adicionarAoCarrinhoPasteis() {
-    var id = document.getElementById("modal-id-pasteis").value;
-    var nome = document.getElementById("modal-nome-pasteis").textContent.replace("Item: ", "");
-    var preco = parseFloat(document.getElementById("modal-preco-pasteis").textContent.replace("Preço: R$", ""));
-    var quantidade = parseInt(document.getElementById("quantidade-pasteis").value);
-    var quantidadeAdicional = parseInt(document.getElementById("quantidade-adicional-pasteis").value);
+    var nome = document.getElementById("modalTitle").textContent;
+    var preco = document.getElementById("modalPrice").textContent.split(" ")[1];
+    var quantidade = document.getElementById("quantity").value;
 
-    // Lógica para adicionar ao carrinho de pasteis
-    var item = {
-        id: id,
-        nome: nome,
-        preco: preco,
-        quantidade: quantidade,
-        adicionais: {
-            quantidade: quantidadeAdicional,
-            precoUnitario: 3.00  // Preço do adicional por unidade
-        }
-    };
-
-    // Adicione o item ao carrinho ou faça qualquer outra lógica desejada
-    // Exemplo: carrinho.push(item);
-
-    // Fechar o modal após adicionar ao carrinho
-    fecharModal('modalPasteis');
+    // Use um nome diferente para a função para evitar a chamada recursiva
+    adicionarItemAoCarrinhoPasteis(nome, preco, quantidade);
+    fecharModalPasteis();
 }
 
-function fecharModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
+var carrinho = [];
+
+function adicionarItemAoCarrinhoPasteis(nome, preco, quantidade) {
+    // Cria um objeto com as informações do pastel
+    var item = {
+        nome: nome,
+        preco: parseFloat(preco),
+        quantidade: parseInt(quantidade),
+        tipo: 'pastel'
+    };
+
+    // Adiciona o pastel ao carrinho
+    carrinho.push(item);
+
+    // Atualiza a exibição do carrinho
+    atualizarCarrinho();
+
+    // Fecha o modal
+    fecharModalPasteis();
+}
+
+function atualizarCarrinho() {
+    var carrinhoContainer = document.getElementById("carrinho-container");
+    carrinhoContainer.innerHTML = "";
+
+    // Itera sobre os itens do carrinho e os exibe
+    for (var i = 0; i < carrinho.length; i++) {
+        var item = carrinho[i];
+
+        var itemDiv = document.createElement("div");
+        itemDiv.classList.add("carrinho-item");
+
+        var nomeDiv = document.createElement("div");
+        nomeDiv.textContent = item.nome;
+
+        var precoDiv = document.createElement("div");
+        precoDiv.textContent = "Preço: R$" + (item.preco * item.quantidade).toFixed(2);
+
+        var quantidadeDiv = document.createElement("div");
+        quantidadeDiv.textContent = "Quantidade: " + item.quantidade;
+
+        itemDiv.appendChild(nomeDiv);
+        itemDiv.appendChild(precoDiv);
+        itemDiv.appendChild(quantidadeDiv);
+
+        carrinhoContainer.appendChild(itemDiv);
+    }
 }
